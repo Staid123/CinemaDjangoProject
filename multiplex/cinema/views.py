f35c6444ec786fa05c0daae6d622792a155beff6
Kirill Pavlenko <pavlenkokirill120@gmail.com>
2024-02-10 16:25:36 +0200

    new view 'show_post'

--- multiplex/cinema/views.py
+++ multiplex/cinema/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from . import services
+from django.shortcuts import get_object_or_404
+from .models import Movie
 
 
 def home_view(request):
+
     """ Главная страница """
+
     movies = services.get_all_movies()
     return render(request, 'cinema/index.html', {'movies': movies})
+
+
+def show_post(request, post_slug):
+
+    """ Подробнее о фильме """
+
+    post = get_object_or_404(Movie, slug=post_slug)
+    return render(request, 'cinema/post.html')
