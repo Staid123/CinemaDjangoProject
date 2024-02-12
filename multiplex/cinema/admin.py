462a4d5ed6dc902bfa2acc35956872a06971435e
Kirill Pavlenko <pavlenkokirill120@gmail.com>
2024-02-12 21:53:38 +0200

    Зарегистрирована модель Session

--- multiplex/cinema/admin.py
+++ multiplex/cinema/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Movie, Genre
+from .models import Movie, Genre, Session
 
 admin.site.register(Movie)
 admin.site.register(Genre)
+admin.site.register(Session)
 
 admin.site.site_header = "Панель администрирования"
