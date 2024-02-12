ed70fe4fc64fa2bcd3779783ccefe7bd15dce124
Kirill Pavlenko <pavlenkokirill120@gmail.com>
2024-02-12 21:57:17 +0200

    Улучшена функция представления поста. Добавлено отображения даты и времени сеансов фильмов

--- multiplex/cinema/views.py
+++ multiplex/cinema/views.py
@@ -17,4 +17,18 @@ def show_post(request, movie_slug):
     """ Подробнее о фильме """
 
     movie = get_object_or_404(Movie, slug=movie_slug)
-    return render(request, 'cinema/post.html', {'movie': movie})
+
+    hours, remainder = divmod(int(movie.duration), 60)
+    movie.duration = f"{hours}:{remainder:02d}"
+
+    movies = services.get_random_movies(movie)
+
+    dates = list(set(movie.session.values_list('date', flat=True).order_by('date')))
+
+    sessions_by_date = {}
+
+    for date in dates:
+        time_list = list(movie.session.filter(date=date).values_list('time', flat=True))
+        sessions_by_date[date] = time_list
+
+    return render(request, 'cinema/post.html', {'movie': movie, 'movies': movies, 'dates': sessions_by_date})
