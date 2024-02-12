aac88fa9f9302d6f3fccba054bfa7a070d04aea5
Kirill Pavlenko <pavlenkokirill120@gmail.com>
2024-02-12 22:00:53 +0200

    Добавлена функция получения последних добавленных фильмов, также улучшена функция переводчик с русского на английский

--- multiplex/cinema/services.py
+++ multiplex/cinema/services.py
@@ -10,7 +10,7 @@ def translit_to_eng(s: str) -> str:
          'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
          'ш': 'sh', 'щ': 'shch', 'ь': '', 'ы': 'y', 'ъ': '', 'э': 'r', 'ю': 'yu', 'я': 'ya', 'й': 'i'}
 
-    return "".join(map(lambda x: d[x] if d.get(x, False) else x, s.lower()))
+    return "".join(map(lambda x: d[x] if x in d.keys() else x, s.lower()))
 
 
 def get_all_movies() -> QuerySet:
@@ -19,3 +19,12 @@ def get_all_movies() -> QuerySet:
     :return: QuerySet
     """
     return models.Movie.objects.all()
+
+
+def get_random_movies(exclude_movie):
+    """
+    Получаем три случайных фильма, исключая переданный movie,
+    с сортировкой по времени создания в обратном порядке
+    """
+    random_movies = models.Movie.objects.exclude(id=exclude_movie.id).order_by('?').order_by('start_of_rental')[:5]
+    return random_movies
