abd22411dba48f9bbaf5a6a960170feab9270c55
Kirill <pavlenkokirill120@gmail.com>
2024-02-24 19:11:10 +0200

    Added new model

--- multiplex/cinema/admin.py
+++ multiplex/cinema/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Movie, Genre, Session, Hall
+from .models import Movie, Genre, Session, Hall, Product
 
 admin.site.register(Movie)
 admin.site.register(Genre)
 admin.site.register(Session)
 admin.site.register(Hall)
+admin.site.register(Product)
 
 admin.site.site_header = "Панель администрирования"
