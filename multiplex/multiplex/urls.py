ae17ae138b30f105e2f8ac4e039faa7d919ccad5
Kirill <pavlenkokirill120@gmail.com>
2024-02-24 19:15:38 +0200

    Новые маршруты для приложения users

--- multiplex/multiplex/urls.py
+++ multiplex/multiplex/urls.py
@@ -22,6 +22,7 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cinema.urls')),
+    path('users/', include('users.urls', namespace="users")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
 
