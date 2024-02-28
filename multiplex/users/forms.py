1e92c66347762390592c62efec888602195b5fb8
Kirill <pavlenkokirill120@gmail.com>
2024-02-28 18:33:27 +0200

    небольшие изменения

--- multiplex/users/forms.py
+++ multiplex/users/forms.py
@@ -43,7 +43,7 @@ class RegisterUserForm(UserCreationForm):
 
 class ProfileUserForm(forms.ModelForm):
     username = forms.CharField(disabled=True, label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    email = forms.CharField(disabled=True, label='E-mail', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.CharField(disabled=True, required=False, label='E-mail', widget=forms.TextInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = get_user_model()
