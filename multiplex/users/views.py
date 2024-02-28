bdc9b35da98bcd3cc735761741f1e450d9fd7e6a
Kirill <pavlenkokirill120@gmail.com>
2024-02-28 10:06:18 +0200

    Добавлены новые классы представления для авторизации, регистрации, смены пароля и отображение профиля пользователя

--- multiplex/users/views.py
+++ multiplex/users/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import LoginView
-from django.views.generic.edit import CreateView
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
-from .forms import LoginUserForm, RegisterUserForm
+from .forms import LoginUserForm, RegisterUserForm, ProfileUserForm, UserPasswordChangeForm
 
 
 class LoginUser(LoginView):
@@ -18,3 +18,20 @@ class RegisterUser(CreateView):
     form_class = RegisterUserForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    form_class = ProfileUserForm
+    template_name = 'users/profile.html'
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=[self.request.user.pk])
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class UserPasswordChange(PasswordChangeView):
+    form_class = UserPasswordChangeForm
+    success_url = reverse_lazy("users:password_change_done")
+    template_name = "users/password_change_form.html"
